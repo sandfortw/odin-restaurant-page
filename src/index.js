108159b5684e3dda57f886e56610ca2063fd262e
Weston Sandfort <sandfortw@gmail.com>
2024-03-29 20:42:42 -0600

    Style pizza image and title

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import pageLoad from "./pageLoad"
+import './style.css'
 
 const content_node = document.querySelector('#content')
 content_node.appendChild(pageLoad)
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -5,15 +5,16 @@ function pageLoadComponent() {
   const div = document.createElement('div')
 
   function headerComponent() {
+    const div = document.createElement('div')
     const header = document.createElement("H1")
     header.textContent = "Weston's Spicy Italian Restaurant"
-  
-    return header
+    let imgNode = new Image()
+    imgNode.src = PizzaPic
+    div.setAttribute("id", "title-container")
+    div.appendChild(imgNode)
+    div.appendChild(header)
+    return div
   }
-
-  let imgNode = new Image()
-  imgNode.src = PizzaPic
-  div.appendChild(imgNode)
   div.appendChild(headerComponent())
   return div
 }
