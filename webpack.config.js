440bbc0276bab7060888c18a87dd103c0aba5dd6
Weston Sandfort <sandfortw@gmail.com>
2024-03-29 19:48:42 -0600

    Prove webpack is working with 'hello world'

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Restaurant PAge',
+      title: 'Restaurant Page',
       filename: 'index.html',
       inject: 'body',
-      
     })
   ],
   module: {
